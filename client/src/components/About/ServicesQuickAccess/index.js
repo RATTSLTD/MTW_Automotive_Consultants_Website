e063a90f113ed9d87cba19eaa140604762bf53aa
RATTSLTD <rattsltd@gmail.com>
2021-07-28 00:14:35 +0100

    fixed links

--- client/src/components/About/ServicesQuickAccess/index.js
+++ client/src/components/About/ServicesQuickAccess/index.js
@@ -16,19 +16,19 @@ const Services = () => {
         <ServicesContainer>
             <ServicesP>MTW Automotive provides development and delivery in the automotive industry to help drive business performance and improvement through a range of training environments:</ServicesP>
             <ServicesWrapper>
-                <ServicesCard to="/training">
+                <ServicesCard to="/services">
                     <ServicesIcon src={F2F}/>
                     <ServicesH2>Technical Training</ServicesH2>
                 </ServicesCard>
-                <ServicesCard to="/training">
+                <ServicesCard to="/services">
                     <ServicesIcon src={RemoteLearning}/>
                     <ServicesH2>Course Development</ServicesH2>
                 </ServicesCard>
-                <ServicesCard to="/training">
+                <ServicesCard to="/services">
                     <ServicesIcon src={RemoteLearning}/>
                     <ServicesH2>Training Needs Assessments</ServicesH2>
                 </ServicesCard>
-                <ServicesCard to="/training">
+                <ServicesCard to="/services">
                     <ServicesIcon src={RemoteLearning}/>
                     <ServicesH2>Customer Service and Management Training</ServicesH2>
                 </ServicesCard>
