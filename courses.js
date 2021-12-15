88cf3053e6d781621761035b9f3ab07c3c20370d
RATTSLTD <rattsltd@gmail.com>
2021-12-15 21:02:17 +0000

    Adding Video batch 1 to courses.js

--- courses.js
+++ courses.js
@@ -6,7 +6,50 @@ module.exports = {
                 "title": "Battery",
                 "url": "https://onedrive.live.com/embed?cid=BF3138943720AE9B&resid=BF3138943720AE9B%2114777&authkey=ALGyzBH2hbQwx7I&em=2",
                 "imgURL": "https://i.postimg.cc/5jnysxyj/FTC-1.png"
+            },
+            {
+                "id": "2",
+                "title": "Cylinder Balance Test",
+                "url": "https://onedrive.live.com/embed?cid=1E5BC56946EE3C27&resid=1E5BC56946EE3C27%211661&authkey=AIBhAxM6sFO0IFA",
+                "imgURL": "https://i.postimg.cc/ncXxSbhF/image.png"
+            },
+            {
+                "id": "3",
+                "title": "Cam Crank Injection Ignition",
+                "url": "https://onedrive.live.com/embed?cid=1E5BC56946EE3C27&resid=1E5BC56946EE3C27%211662&authkey=AHTii0558JyxTAg",
+                "imgURL": "https://i.postimg.cc/wvM4xzP3/image.png"
+            },
+            {
+                "id": "4",
+                "title": "Cam Crank Injection Ignition 2",
+                "url": "https://onedrive.live.com/embed?cid=1E5BC56946EE3C27&resid=1E5BC56946EE3C27%211663&authkey=APkEyUOM0nhd60Y",
+                "imgURL": "https://i.postimg.cc/NjGzCR41/image.png"
+            },
+            {
+                "id": "5",
+                "title": "ABS Sensor",
+                "url": "https://onedrive.live.com/embed?cid=1E5BC56946EE3C27&resid=1E5BC56946EE3C27%211667&authkey=AJQMZSH88pAIIYE",
+                "imgURL": "https://i.postimg.cc/PqpK0y88/image.png"
+            },
+            {
+                "id": "6",
+                "title": "MAP Sensor",
+                "url": "https://onedrive.live.com/embed?cid=1E5BC56946EE3C27&resid=1E5BC56946EE3C27%211666&authkey=AA4xkd-Zq4bOh3M",
+                "imgURL": "https://i.postimg.cc/445vLqFS/image.png"
+            },
+            {
+                "id": "7",
+                "title": "Crank Case Pressure",
+                "url": "https://onedrive.live.com/embed?cid=1E5BC56946EE3C27&resid=1E5BC56946EE3C27%211665&authkey=AFZK6fjHyqEyKlY",
+                "imgURL": "https://i.postimg.cc/BQzKR05M/image.png"
+            },
+            {
+                "id": "8",
+                "title": "Exhaust Pressure with Misfire",
+                "url": "https://onedrive.live.com/embed?cid=1E5BC56946EE3C27&resid=1E5BC56946EE3C27%211664&authkey=AJudqFvDNQm63Ao",
+                "imgURL": "https://i.postimg.cc/xCj8cxyX/image.png"
             }
+
         ],
 
         paid:[
